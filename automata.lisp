05ab4609ddb248d6e1f6aeb9464cff32706748a5
wmealing <wmealing@gmail.com>
2020-12-18 03:25:41 +1000

    First real working state, too bad it needs streams.

--- automata.lisp
+++ automata.lisp
@@ -1,14 +1,14 @@
-;;;; automata.lisp
-
+;; automata.lisp
 
 (ql:quickload "str")
 (ql:quickload "cl-arrows")
 
 (defpackage :automata
   (:use :cl :cl-arrows)
-  (:export :main :read-db :parse-lines :filter-lines :sw-bus
-   :select-by-function
-   ))
+  (:export :main
+           :parse-lines :filter-lines :sw-bus
+           :reload :select-by-function :find-func-in-all-tags
+           :🆑))
 
 (in-package #:automata)
 
@@ -20,6 +20,7 @@
   (str:lines data))
 
 (defun sw-bus (li)
+  "Starts with bang underscore"
   (str:starts-with? "!_" li))
 
 (defun filter-lines (data-lines)
@@ -27,6 +28,7 @@
   (remove-if #'sw-bus data-lines))
 
 (defun make-entry ( name file start end )
+  "Turns parameters into a map /vector ?"
   (list :func-name name :file file :start start :end end))
 
 (defun get-val (s)
@@ -37,56 +39,85 @@
   (- (get-val v) 1)
   )
 
-(defun parse-lines (d)
+(defun parse-lines (dir line)
   "Parse the line into more usable terms"
   (mapcar (lambda (arg)
-            (let ((sl (str:split #\Tab arg)))
-              (make-entry (first sl)
-                          (second sl)
-                          (fix-val (fifth sl))
-                          (get-val (seventh sl))
-                          )
+            (let* ((sl (str:split #\Tab arg))
+                   (func-name (first sl))
+                   (file-name (second sl))q
+                   (start (fix-val (fifth sl)))
+                   (end (get-val (seventh sl)))
+                   (full-path (str:join "" (list dir file-name)))
+                  )
+              (make-entry func-name full-path start end)
               )
-            )d))
-
+            )line))
 
 (defun select-by-function (func-name db)
-  (format t "running~%")
   (remove-if-not
    #'(lambda (entry) (equal (getf entry :func-name) func-name)) db))
 
 
-(defun load-database ()
-  (-<> "/Users/wmealing/Projects/automata/tests/tags"
+(defun load-database (tagfile)
+  (let ((dir (directory-namestring tagfile)))
+  (-<> tagfile
        (read-db <>)
        (split-lines <>)
        (filter-lines <>)
-       (parse-lines <>)
-       )
-  )
+       (parse-lines dir <>)
+       )))
 
-(defun print-function-match-debug (match)
-  (format t "=========================== ~%" )
-  (format t "Function      : ~a ~%" (getf (first d) :func-name))
-  (format t "Starts at line: ~a ~%" (getf (first d) :start))
-  (format t "Ends at line  : ~a ~%" (getf (first d) :end))
-  (format t "=========================== ~%" )
-  )
-
-(defun extract-function (filename line-start line-end)
-  (let (( contents (str:lines (str:from-file filename)))
-        )
-    (subseq contents line-start line-end)
-    )
-  )
+(defun extract-function (file-name line-start line-end)
+  (subseq
+   (str:lines (str:from-file file-name)) line-start line-end))
 
 (defun make-function-printable(contents)
-  (join #\Newline contents)  )
+  (str:join #\Newline contents))
+
+(defun get-releases-tags-files()
+  "Returns a list of 'tags files for releases "
+  (directory "./code/*/tags"))
+
+(defun find-func-in-all-tags (function)
+  (let ( (tags (get-releases-tags-files) ) )
+    (mapcar (lambda (tagfile)
+              (->> tagfile
+                   (load-database)
+                   (select-by-function function)
+                   (first) ;; why ?
+                   )
+              ) tags
+                )))
+
+(defun print-function (f)
+  (let* (
+         (file-name (getf f :file))
+         (line-start (getf f :start))
+         (line-end (getf f :end))
+         (func-data (extract-function file-name line-start line-end) )
+         )
+    (format t "== START SOURCE: ~a ==" file-name )
+    (format t "~%~a~%" (str:join #\Newline func-data))
+    (format t "== END SOURCE ==~%~%")
+    nil
+    ))
+
+(defun print-all-functions (f)
+  (format t "Looking for function: -= ~a =- ~%" "main")
+  (mapcar #'print-function (find-func-in-all-tags "main"))
+  )
 
 (defun main ()
-;;;  (print (load-database ))
-  (setf d (select-by-function "another_one" (load-database)))
-  (print-function-match-debug d)
-  (format t "~a ~%" (make-function-printable (extract-function "tests/test.c" 6 10 )))
-  T
+  "The main function"
+  (print-all-functions "main")
   )
+
+
+(defun build-tags ()
+  (-<> (directory "./code/*")
+       (print <>)
+   ))
+
+(defun reload()
+  (ql:quickload "automata")
+  (automata:main))
