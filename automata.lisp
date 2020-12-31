3a687794292bf6d4900f144ade24ad6d5e8f23f0
wmealing <wmealing@gmail.com>
2021-01-01 00:30:36 +1000

    Major refactor to deal with changes in requirements.

--- automata.lisp
+++ automata.lisp
@@ -2,17 +2,16 @@
 
 (ql:quickload "str")
 (ql:quickload "cl-arrows")
-(ql:quickload "fiveam")
-(ql:quickload "gtwiwtg")
 
 (defpackage :automata
-  (:use :cl :cl-arrows :gtwiwtg)
+  (:use :cl :cl-arrows )
   (:export :main :line-matches :find-in-tags-file :line-to-structure
-           :run-tests :get-releases-tags-files
-           :get-all-tag-files :reload
-           :select-by-function :find-func-in-all-tags))
+           :run-tests :get-releases-tags-files :format-function
+   :get-all-tag-files :return-all-matches :extract-function :wrap-in-p
+   :select-by-function :find-func-in-all-tags))
 
 (in-package #:automata)
+
 
 (defun starts-with-bang-underscore(li)
   "Starts with bang underscore"
@@ -32,13 +31,15 @@
   )
 
 (defun split-kv(kvpair)
-  (parse-integer (second (str:split  ":" kvpair)))
+  (format t "SPLIT-KV ~a ~%" kvpair)
+  (let ((key-value (str:split  ":" kvpair)))
+    (parse-integer (second key-value))
+    )
   )
 
 ;;; this function is NOT robust.
 (defun line-to-structure (line tagfile)
   "Takes a line and returns it back as a well formatted data structure"
-  (print tagfile)
   (if (not (starts-with-bang-underscore line))
       (let* ((elements (str:split #\Tab line))
              (func-name (first elements))
@@ -47,24 +48,55 @@
              (file-path (merge-pathnames (pathname file-name) tagfile-base))
              (start     (split-kv (fifth elements)) )
              (end       (split-kv (seventh elements))))
-        (list :file file-path :func func-name :start start :end end))
-      ))
+        (format t "ELEMENTS~a~%"  elements)
+        (format t "FUNC_NAME: ~a~%" func-name)
+        (format t "file-path: ~a~%" file-path)
+        (format t "start: ~a~%" start)
+        (format t "end: ~a~%" end)
+        `(:file ,file-path :func ,func-name :start ,start :end ,end))))
+
+
+(defun get-release-from-tagfile (tagfile)
+  "Release file is in the same dir as tagfile, grab contents of that"
+  (let* (    (tagfile-base (directory-namestring tagfile))
+             (file-path (merge-pathnames (pathname "release") tagfile-base))
+             )
+    (str:from-file file-path)))
+
 
 (defun find-in-tags-file (tagfile func-name)
   "Find file in the tagfile this way because tagfiles can be huge."
-  (with-open-file (stream tagfile)
-    (loop for line = (read-line stream nil)
-          while line do
-            (if (eq T (line-matches line func-name))
-                (progn
-                  (return (line-to-structure line tagfile ))
-                  )
+
+  (list :release (get-release-from-tagfile tagfile) :match-list
+        (with-open-file (stream tagfile)
+          (loop for line = (read-line stream nil)
+                while line
+                when (eq T (line-matches line func-name))
+                  collect (line-to-structure line tagfile)
                 )
-          )))
+          )
+        )
+  )
 
 (defun extract-function (file-name line-start line-end)
-  (subseq
-   (str:lines (str:from-file file-name)) line-start line-end))
+  (if (and file-name line-start line-end)
+      (subseq
+       ;; line-start - -1, because indexing here starts at 0.
+       (str:lines (str:from-file file-name)) (- line-start 1) line-end))
+  )
+
+(defun extract-source (m)
+  "Given a set struct of file start and end, extract that source and return it"
+  (format t "EXTRACTING SOURCE ~a~%" m)
+  (let* (
+         (file-path (getf m :file ))
+         (start-pos (getf m :start ))
+         (end-pos   (getf m :end ))
+         )
+    (if (and file-path start-pos end-pos)
+        (extract-function file-path start-pos end-pos )
+        )
+    ))
 
 (defun make-function-printable(contents)
   (str:join #\Newline contents))
@@ -73,45 +105,23 @@
   "Returns a list of 'tags files for releases "
   (directory "./code/*/tags"))
 
-(defun find-func-in-all-tags (function)
-  (progn
-    (format t "Looking for function: ~a~%" function )
-    (mapcar (lambda (tagfile)
-            (find-in-tags-file tagfile function)) (get-all-tag-files) )
-    )
+(defun find-func-in-all-tags (func-name)
+  (mapcar (lambda (tagfile)
+            (find-in-tags-file tagfile func-name)) (get-all-tag-files)))
+
+(defun format-function (match-data)
+  (if match-data
+      (list :release (getf match-data :release )
+            :match-list
+            (loop for el in (getf match-data :match-list )
+                  collect (extract-source el)
+                  ))))
+
+
+(defun return-all-matches (func-name)
+  (mapcar #'format-function (find-func-in-all-tags func-name))
   )
-
-
-(defun print-function (f)
-  (if (not (eq f nil) )
-      (let* (
-             (file-name (getf f :file))
-             (line-start (- (getf f :start) 1)) ;; indexing change.
-             (line-end  (getf f :end) ) ;; include the ending brace ?
-             (func-data (extract-function file-name line-start line-end))
-             )
-        (progn
-          (format t "~%== START SOURCE: ~a Lines: ~a-~a==" file-name line-start line-end )
-          (format t "~%~a~%" (str:join #\Newline func-data))
-          (format t "== END SOURCE ==~%~%")
-          )
-        )
-      (format t  "Function to print is nil? : ~a~% " f)
-      ))
-
-(defun print-all-functions (f)
-  (format t "Looking for function: -= ~a =- ~%" f)
-  (mapcar #'print-function (find-func-in-all-tags f)))
 
 (defun main ()
-  "The main function"
-  (print-all-functions "main")
-;;  (find-func-in-all-tags "main")
-  )
-
-(defun reload ()
-  (ql:quickload "automata")
-  (ql:quickload 'automata/tests))
-
-(defun run-tests ()
-  (automata-tests::test-automata))
+  (print (find-func-in-all-tags "main"))
+  nil)
