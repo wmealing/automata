87889b4c491d4c5868cac7ea909a7e5418c3a07c
wmealing <wmealing@gmail.com>
2021-02-08 16:39:41 +1000

    Working.

--- automata.lisp
+++ automata.lisp
@@ -1,10 +1,12 @@
 ;; automata.lisp
 
 (ql:quickload "str")
-(ql:quickload "cl-arrows")
+(ql:quickload "split-sequence")
+;; (ql:quickload "trivia")
+
 
 (defpackage :automata
-  (:use :cl :cl-arrows )
+  (:use :cl)
   (:export :main :line-matches :find-in-tags-file :line-to-structure
            :run-tests :get-releases-tags-files :format-function
    :get-all-tag-files :return-all-matches :extract-function :wrap-in-p
@@ -22,38 +24,63 @@
 
 (defun line-matches (line func-name)
   "Takes a line from ctags file and tests to see if the line starts with..."
-  (let ((elements (str:split #\Tab line)))
-    (if (not (str:starts-with? "!_" (nth 0 elements)))
-        (if (string= (nth 0 elements) func-name)
+  (let ((elements (split-sequence:split-sequence #\Tab line)))
+    (if (not (str:starts-with? "!_" (elt elements 0)))
+        (if (string= (elt elements 0) func-name)
             T)
         nil)
     )
   )
 
 (defun split-kv(kvpair)
-  (format t "SPLIT-KV ~a ~%" kvpair)
-  (let ((key-value (str:split  ":" kvpair)))
-    (parse-integer (second key-value))
-    )
-  )
+  "Splits a string in :"
+  (let ((key-value (str:split ":" kvpair)))
+    (if key-value
+        (parse-integer (second key-value))
+        )))
+
+
+(defun find-v-kv (line key)
+  "Find a value by key in a line"
+
+  (loop for e in (split-sequence:SPLIT-SEQUENCE #\Tab line)
+        do (format t "Iterating over: ~a ~%" e )
+        when (str:starts-with? (str:concat key ":") e)
+          return (values (split-kv e))
+        ))
+
+
+
+(defun find-v-kv-orig (line key)
+  "Find a value by key in a line"
+  (if line
+      (mapc (lambda (element)
+              (format t "KEY: ~a~%" key)
+              (format t "ELEMENT: ~a~%" element)
+              (let ((newprefix (str:concat key ":")))
+                (if (str:starts-with? newprefix element)
+                    (split-kv element)
+                    ))
+              )
+            (split-sequence:SPLIT-SEQUENCE #\Space line)
+            )))
+
 
 ;;; this function is NOT robust.
 (defun line-to-structure (line tagfile)
   "Takes a line and returns it back as a well formatted data structure"
-  (if (not (starts-with-bang-underscore line))
-      (let* ((elements (str:split #\Tab line))
-             (func-name (first elements))
-             (tagfile-base (directory-namestring tagfile))
-             (file-name (second elements))
-             (file-path (merge-pathnames (pathname file-name) tagfile-base))
-             (start     (split-kv (fifth elements)) )
-             (end       (split-kv (seventh elements))))
-        (format t "ELEMENTS~a~%"  elements)
-        (format t "FUNC_NAME: ~a~%" func-name)
-        (format t "file-path: ~a~%" file-path)
-        (format t "start: ~a~%" start)
-        (format t "end: ~a~%" end)
-        `(:file ,file-path :func ,func-name :start ,start :end ,end))))
+  (format t "LINE: ~a~%" line)
+  (unless (starts-with-bang-underscore line)
+    (let* ((elements (str:split #\Tab line))
+           (func-name (first elements))
+           (tagfile-base (directory-namestring tagfile))
+           (file-name (second elements))
+           (file-path (merge-pathnames (pathname file-name) tagfile-base))
+           (start (find-v-kv line "line")) ;; this is broken
+           (end (find-v-kv line "end")) ;; this is broken
+           )
+
+      `(:file ,file-path :func ,func-name :start ,start :end ,end))))
 
 
 (defun get-release-from-tagfile (tagfile)
@@ -61,22 +88,27 @@
   (let* (    (tagfile-base (directory-namestring tagfile))
              (file-path (merge-pathnames (pathname "release") tagfile-base))
              )
+
     (str:from-file file-path)))
+
+;; this is much, much faster.
+;;
+
 
 
 (defun find-in-tags-file (tagfile func-name)
   "Find file in the tagfile this way because tagfiles can be huge."
+  (let* (
+         (all-output
+           (uiop:run-program (list "readtags" "-t" (namestring tagfile)  "-p" "-ne" "-" func-name) :output :string))
+         (output-list (str:split #\Newline all-output :omit-nulls t))
+         )
 
-  (list :release (get-release-from-tagfile tagfile) :match-list
-        (with-open-file (stream tagfile)
-          (loop for line = (read-line stream nil)
-                while line
-                when (eq T (line-matches line func-name))
-                  collect (line-to-structure line tagfile)
-                )
-          )
-        )
-  )
+    `(:release ,(get-release-from-tagfile tagfile) :match-list
+               ,(loop for line in output-list
+                      collect (line-to-structure line tagfile)
+                      ))
+    ))
 
 (defun extract-function (file-name line-start line-end)
   (if (and file-name line-start line-end)
@@ -116,7 +148,6 @@
             (loop for el in (getf match-data :match-list )
                   collect (extract-source el)
                   ))))
-
 
 (defun return-all-matches (func-name)
   (mapcar #'format-function (find-func-in-all-tags func-name))
